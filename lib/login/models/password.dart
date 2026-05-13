a14b63e93fb34c6ccaefd512c52ca1deca7406db
Riki-prayuda <rikiprayuda104@gmail.com>
2026-05-13 13:41:44 +0700

    Update password.dart

---
+++ lib/login/models/password.dart
@@ -0,0 +1,28 @@
+import 'package:formz/formz.dart';
+
+enum PasswordValidationError {
+  invalid,
+}
+
+class Password
+    extends FormzInput<
+        String,
+        PasswordValidationError> {
+
+  const Password.pure()
+      : super.pure('');
+
+  const Password.dirty(
+      [super.value = ''])
+      : super.dirty();
+
+  @override
+  PasswordValidationError?
+      validator(String value) {
+
+    return value.length >= 6
+        ? null
+        : PasswordValidationError
+            .invalid;
+  }
+}
